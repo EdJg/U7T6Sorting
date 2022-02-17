1688cf0459b75ba89c00caf36cc9d478fcf52040
BT_1E10_25 <sample@bths.edu>
2022-02-17 13:59:12 -0500

    Updated class

--- src/Sort.java
+++ src/Sort.java
@@ -3,8 +3,9 @@ import java.util.ArrayList;
 public class Sort
 {
   // Part A.  Which Sort is Which?
-  public static void firstSortingAlgorithm(int[] elements)
+  public static void selectionSort(int[] elements)
   {
+    int count = 0;
     for (int j = 0; j < elements.length - 1; j++)
     {
       int minIndex = j;
@@ -14,16 +15,19 @@ public class Sort
         {
           minIndex = k;
         }
+        count ++;
       }
       int temp = elements[j];
       elements[j] = elements[minIndex];
       elements[minIndex] = temp;
     }
+    System.out.println("Iterations: " + count);
   }
 
   // Part A.  Which Sort is Which?
-  public static void secondSortingAlgorithm(int[] elements)
-  { 
+  public static void insertionSort(int[] elements)
+  {
+    int count = 0;
     for (int j = 1; j < elements.length; j++)
     {
       int temp = elements[j];
@@ -32,20 +36,51 @@ public class Sort
       {     
         elements[possibleIndex] = elements[possibleIndex - 1];
         possibleIndex--;
+        count ++;
       }
       elements[possibleIndex] = temp;
     }
+    System.out.println("Iterations: " + count);
   }
   
   // Part C.  Sorting a 1000-word list!
   public static void selectionSortWordList(ArrayList<String> words)
   {
-    /* TO BE IMPLEMENTED IN PART C */
+    int count = 0;
+    for (int index = 0; index < words.size() - 1; index++)
+    {
+      int least = index;
+      for (int index2 = index + 1; index2 < words.size(); index2++)
+      {
+        if (words.get(index2).compareTo(words.get(index)) > 0)
+        {
+          least = index2;
+        }
+        count++;
+      }
+      String stuff = words.get(index);
+      words.set(index, words.get(least));
+      words.set(least, stuff);
+    }
+    System.out.println("Iterations: " + count);
   }
 
   // Part C.  Sorting a 1000-word list!
   public static void insertionSortWordList(ArrayList<String> words)
   {    
-    /* TO BE IMPLEMENTED IN PART C */
+    int count = 0;
+    for (int index = 1; index < words.size(); index ++)
+    {
+      String temp = words.get(index);
+      int maybe = index;
+      while (maybe > 0 && temp.compareTo(words.get(index - 1)) > 0)
+      {
+        words.set(index, words.get(index - 1));
+        maybe --;
+        count ++;
+      }
+      words.set(maybe, temp);
+    }
+    System.out.println("Iterations: " + count);
   }
 }
